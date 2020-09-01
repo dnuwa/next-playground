a06ce2c991b6bb6ee7764bc77a9675f167dccbaf
dnuwa <daniel.nuwa@gmail.com>
2020-09-01 22:22:47 +0300

    - setup a next.js app - install dependecies to create a simple reate app

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Layout from '../containers/Layout'
+
+const about = () => {
+    return (
+        <Layout>
+            <div>
+                <h1>About BitzPrices</h1>
+                <p>Application to view bit coin prices</p>
+            </div>
+        </Layout>
+    )
+}
+
+export default about;
